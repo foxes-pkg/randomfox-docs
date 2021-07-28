ab0e1eaca0d93858bea6c68d96c27d616bc6a888
Minecodes <6444823-Minecodes13@users.noreply.gitlab.com>
2021-07-28 19:40:31 +0200

    Web docs created

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/foxes-pkg/randomfox-docs',
+  titleSuffix: ' – RANDOMFOX',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">randomfox.</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        A content randomizer
       </span>
     </>
   ),
@@ -15,13 +15,13 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="randomfox: a content randomizer" />
+      <meta name="og:description" content="randomfox: a content randomizer" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="randomfox: A content randomizer" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
@@ -56,10 +56,10 @@ export default {
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
-  search: true,
-  prevLinks: true,
-  nextLinks: true,
+  search: false,
+  prevLinks: false,
+  nextLinks: false,
   footer: true,
-  footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerEditOnGitHubLink: false,
+  footerText: <>MIT {new Date().getFullYear()} © Foxes PKG</>,
 }
